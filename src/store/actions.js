4f5410d79571afb819280e759a4a51b6a74c9ac7
souths <souths@qq.com>
2017-08-21 17:38:25 +0800

    删除显示别人id问题

--- src/store/actions.js
+++
@@ -1,5 +0,0 @@
-import {USER_SIGNIN,USER_SIGNOUT} from './types'
-export default{
-  
-
-}
--- src/store/types.js
+++
@@ -1,2 +0,0 @@
-export const USER_SIGNIN = 'USER_SIGNIN' //登录成功
-export const USER_SIGNOUT = 'USER_SIGNOUT' //退出登录
